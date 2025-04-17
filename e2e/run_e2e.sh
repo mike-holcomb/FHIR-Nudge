d63bca5022112ed0be1395e29118b7222c117c1e
Michael Holcomb <michael.holcomb@utsouthwestern.edu>
2025-04-17 10:22:44 -0500

    Remove default FHIR server URL from E2E test script

--- e2e/run_e2e.sh
+++ e2e/run_e2e.sh
@@ -7,12 +7,6 @@ set -e
 echo "Setting PYTHONPATH to project root..."
 export PYTHONPATH="$(dirname $(dirname $(realpath $0)))"
 
-# Set FHIR_SERVER_URL if not already set
-if [ -z "$FHIR_SERVER_URL" ]; then
-  echo "FHIR_SERVER_URL not set. Using default: http://hapi.fhir.org/baseR4"
-  export FHIR_SERVER_URL="http://hapi.fhir.org/baseR4"
-fi
-
 # Run the E2E tests
 echo "Running E2E tests..."
 poetry run python $(dirname $0)/e2e_runner.py
